00aee743dc80985cdf88ba270f2172c9f8af8155
hiroyasu-takubo <hiro9646@gmail.com>
2016-02-10 23:11:29 +0900

    staic page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+# coding: utf-8
 module ApplicationHelper
+
+  #ページごとの完全なタイトルを返す。
+  def full_title(page_title = '')
+    base_title = "tome glimpse"
+
+    if page_title.empty?
+      base_title
+    else
+      page_title + "|" + base_title
+    end
+  end
+
+  
 end
