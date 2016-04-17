b89f141a4de732233dcfda5ddf051599feef3cbe
hiroyasu-takubo <hiro9646@gmail.com>
2016-04-17 22:18:27 +0900

    modify index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,24 +9,35 @@ class BooksController < ApplicationController
       flash.now[:danger] = '本の名前を入力してください'
       render 'new'
     else
-      @res = Amazon::Ecs.item_search(item_name,
+     res = Amazon::Ecs.item_search(item_name,
                                      :search_index   => 'Books',
                                      :response_group => 'Medium',
                                      :country        => 'jp'
 
                                      )
       @books = []
-      @res.item.each do |item|
+      res.items.each do |item|
         book = Book.new
         book.title = item.get('ItemAttributes/Title')
         book.author = item.get('ItemAttributes/Author')
         book.publisher = item.get('ItemAttributes/Publisher')
-        book.image = item.get('MediumImage/URL') 
+        book.image = item.get('MediumImage/URL')
+
+        @books << book
       end
     end
   end
 
   def new
+  end
+
+  def show
+    @book = Book.new
+    
+    book.title = params[:title]
+    book.author = params[:author]
+    book.publisher = params[:publisher]
+    book.image = params[:image]
   end
 
   def booksearch
