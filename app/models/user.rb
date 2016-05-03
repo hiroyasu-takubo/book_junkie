850fa27cfd7adc2902f787e3a7cd06074e28bba3
hiroyasu-takubo <hiro9646@gmail.com>
2016-05-03 15:35:20 +0900

    タグ登録時にユーザーIDが入るように修正。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessor :remember_token
   has_many :microposts
   has_many :books
+  has_many :tags
   before_save { self.email = email.downcase }
 
   validates :name,  presence: true, length: { maximum: 50 }
