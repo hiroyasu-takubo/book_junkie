45900aaaae9dba08f5d27718247882f19861f515
hiroyasu-takubo <hiro9646@gmail.com>
2016-05-03 17:57:30 +0900

    delete book model colmun tag_id

--- db/migrate/20160417063855_create_books.rb
+++ db/migrate/20160417063855_create_books.rb
@@ -2,7 +2,6 @@ class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
       t.integer :user_id
-      t.integer :tag_id
       t.string :asin
       t.string :title
       t.string :author
@@ -12,6 +11,5 @@ class CreateBooks < ActiveRecord::Migration
       t.timestamps null: false
     end
     add_index :books, [:user_id]
-    add_index :booktags, [:user_id, :tag_id]
   end
 end
