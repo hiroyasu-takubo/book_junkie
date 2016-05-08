d5b59aa9715df584d1126637a96236633066fff9
hiroyasu-takubo <hiro9646@gmail.com>
2016-05-08 15:42:43 +0900

    add book create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,35 +12,49 @@ class BooksController < ApplicationController
   end
 
   def create
-    # @book = Book.new(book_params)
-    # if @book.save
-    # else
-    #   render 'new'
-    # end
+    # TODO 検索方法を帰る。create_orupdateがいい？ 検索ならasinがいい。
+    # TODO タグの削除方法の設計
+    @book = Book.find_by(title: book_params[:title], user_id: current_user.id)
     
-    # @booktags = @book.booktags.build(tag_params[:tag_ids])
-    # @booktags << @book.booktags.build(name: 'all')
+    if @book.blank?
+      @book = Book.new(book_params)
+      
+      unless @book.save
+        flash[:danger] = "登録が失敗しました。"
+        render 'new'
+      end
+    end
     
-    # @booktags.each do |booktag|
-    #   unless booktag.save
-    #     render 'new'
-    #   end
-    # end
+    tag_ids = params[:tag][:tag_ids].select(&:present?)
     
-    # flash[:success] = "本を登録しました。"
-    # redirect_to @book
-    render text: params
+    tag_all = Tag.find_by(user_id: current_user.id, name: 'all')
+    tag_ids << tag_all[:id].to_s
+    
+    tag_hashkey = [:tag_id] * tag_ids.size
+    tag_hash_array = tag_hashkey.collect.zip(tag_ids)
+    tag_hash = []
+    tag_hash_array.each { |h| tag_hash << Hash[*h]}
+    
+    @booktags = @book.booktags.build(tag_hash)
+    
+    @booktags.each do |booktag|
+      unless booktag.save
+        render 'new'
+      end
+    end
+    
+    flash[:success] = "本を登録しました。"
+    redirect_to @book
   end
 
   def show
     @book = Book.find(params)
   end
 
+  private
+
   def book_params
-    params.require(:book).permit(:title,:author, :publisher, :image)
+    params.require(:book).permit(:title,:author, :publisher, :image, { tag_ids: [] })
   end
 
-  def tags_params
-    params.require(:book).permit({ tag_ids: [] })
-  end
 end
