8caa095b3ed5b6aa1a6d0ac263ca7c41bb332681
hiroyasu-takubo <hiro9646@gmail.com>
2016-06-21 23:02:36 +0900

    add book integration test

--- test/integration/book_create_test.rb
+++ test/integration/book_create_test.rb
@@ -2,52 +2,29 @@ require 'test_helper'
 
 class BookCreateTest < ActionDispatch::IntegrationTest
 
-  # def setup
-  #     @user = users(:hiro)
-  # end
+  def setup
+    @user = users(:hiro)
+    @book = books(:Ruby)
+  end
 
-  # test 'should create book' do
-  #   log_in_as(@user)
-  #   get new_book_path
-  #   assert_template 'books/new'
-
-  #   assert_difference 'Book.count', 1 do
-  #     post book_path, book: { asin: '',
-  #                             title: 'Ruby book',
-  #                             author: 'foo',
-  #                             publisher: 'test company',
-  #                             image: 'aaa',
-  #                           }
-  #   end
-  #   assert_template 'books/show'
-  # end
-
-  # test 'should create book' do
-  #   log_in_as(@user)
-  #   get new_book_path
-  #   assert_template 'books/new'
-
-  #   assert_difference 'Book.count', 1 do
-  #     post book_path, book: { asin: '',
-  #                             title: 'Ruby book',
-  #                             author: 'foo',
-  #                             publisher: 'test company',
-  #                             image: 'aaa',
-  #                           }
-  #   end
-  #   assert_template 'books/show'
-  # end
-
-  # test "unsuccesful edit" do
-  #   log_in_as(@user)
-  #   get edit_user_path(@user)
-  #   assert_template 'users/edit'
-  #   patch user_path(@user), user: { name:  "",
-  #                                   email: "foo@invalid",
-  #                                   password:              "foo",
-  #                                   password_confirmation: "bar" }
-  #   assert_template 'users/edit'
+  test 'should create book' do
+    log_in_as(@user)
     
-  # end
-
+    get new_book_path, search: { title: 'Ruby',
+                                 author: 'author',
+                                 publisher: 'publisher',
+                                 image: 'http://dummyimage.com/100x100.jpg'
+                               }
+    assert_template 'books/new'
+    
+    assert_difference 'Book.count', 1 do
+      post_via_redirect books_path, book: { asin: '',
+                                            title: 'Ruby',
+                                            author: 'author',
+                                            publisher: 'publisher',
+                                            image:  'http://dummyimage.com/100x100.jpg'
+                                          }
+    end
+    assert_template 'books/show'
+  end
 end
